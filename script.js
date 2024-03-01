98d62e660eb72ff2415c14066192cde7a5752e3d
ritustus <rkerkovits@gmail.com>
2024-03-01 14:39:55 +0100

    finish

--- script.js
+++ script.js
@@ -30,6 +30,22 @@ document.getElementById("btn-four").addEventListener("click", function(){
 });
 
 
+document.getElementById("question-1").addEventListener("click", function(){
+    switchImage(document.getElementById("btn-one"), document.getElementById("plus-img-1"), document.getElementById("content-1"));
+});
+
+document.getElementById("question-2").addEventListener("click", function(){
+    switchImage(document.getElementById("btn-two"), document.getElementById("plus-img-2"), document.getElementById("content-2"));
+});
+
+document.getElementById("question-3").addEventListener("click", function(){
+    switchImage(document.getElementById("btn-three"), document.getElementById("plus-img-3"), document.getElementById("content-3"));
+});
+
+document.getElementById("question-4").addEventListener("click", function(){
+    switchImage(document.getElementById("btn-four"), document.getElementById("plus-img-4"), document.getElementById("content-4"));
+});
+
 
 
 
